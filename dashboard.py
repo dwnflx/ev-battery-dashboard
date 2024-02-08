22303dcba0464d26d4de754ee4bfa3418bac02c5
aimfeld <adrian.imfeld@brightanswer.ch>
2024-02-08 11:20:45 +0100

    removed unused dependency

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import plotly.express as px
 from sklearn.linear_model import LinearRegression
