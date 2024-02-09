dfa46cc70747a456ea8889068fcd18e7ae503b06
aimfeld <adrian.imfeld@brightanswer.ch>
2024-02-09 10:50:35 +0100

    mining as parameter in kt

--- model.py
+++ model.py
@@ -11,16 +11,16 @@ NEW_FINDS_PERCENT = 0.05
 
 @dataclass
 class Params:
+    # Mineral use in kt
+    battery_production: float
+    mining: float
+
     # Rates between 0 and 1
     battery_recycling_rate: float
     battery_repurpose_rate: float
     battery_waste_rate: float
     grid_recycling_rate: float
     grid_waste_rate: float
-    extraction_limit: float
-
-    # Mineral used in battery production in kt
-    battery_production: float
 
 
 # Initial values in kt
@@ -45,6 +45,8 @@ class BatteryModel():
         self.waste = self.model.stock("waste")
 
         # Flows
+        self.new_finds = self.model.flow("new_finds")
+        self.mining = self.model.flow("mining")
         self.battery_production = self.model.flow("battery_production")
         self.battery_recycling = self.model.flow("battery_recycling")
         self.battery_repurpose = self.model.flow("battery_repurpose")
@@ -53,9 +55,6 @@ class BatteryModel():
         self.grid_waste = self.model.flow("grid_waste")
 
         # Constants
-        self.new_finds = self.model.constant("new_finds")
-        self.mining = self.model.constant("mining")
-
         self.battery_recycling_rate = self.model.constant("battery_recycling_rate")
         self.battery_repurpose_rate = self.model.constant("battery_repurpose_rate")
         self.battery_waste_rate = self.model.constant("battery_waste_rate")
@@ -70,6 +69,8 @@ class BatteryModel():
         self.waste.equation = self.battery_waste + self.grid_waste
 
         # Flow equations
+        self.new_finds.equation = self.resources.initial_value * NEW_FINDS_PERCENT
+        self.mining.equation = params.mining
         self.battery_production.equation = params.battery_production
         self.battery_recycling.equation = self.battery_recycling_rate * self.batteries
         self.battery_repurpose.equation = self.battery_repurpose_rate * self.batteries
@@ -89,9 +90,7 @@ class BatteryModel():
         self.battery_repurpose_rate.equation = params.battery_repurpose_rate
         self.battery_waste_rate.equation = params.battery_waste_rate
         self.grid_recycling_rate.equation = params.grid_recycling_rate
-
-        self.new_finds.equation = self.resources.initial_value * NEW_FINDS_PERCENT
-        self.mining.equation = self.resources.initial_value * params.extraction_limit / (END_YEAR - START_YEAR)
+        self.grid_waste_rate.equation = params.grid_waste_rate
 
 
     def get_stocks_df(self) -> pd.DataFrame:
